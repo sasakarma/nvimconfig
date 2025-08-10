f05400a45f18b01876ecc2fd35d5a4c71466572b
sasakarma <sakashun.ps@gmail.com>
2025-08-11 00:47:16 +0900

    Added config for nvimtree

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,11 @@ return {
     end,
   },
 
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = require "configs.nvimtreeconfig",
+  },
+
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
